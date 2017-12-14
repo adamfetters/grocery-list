f11b8dfd219cd160a74f7e47914dba246c5e4545
Adam Fetters <adamfetterscoder@gmail.com>
2017-12-14 18:33:21 -0500

    React Router Setup with HomePage and Login

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension":0,
+        "no-use-before-define": 0,
+    },
+    "env": {
+        "browser": true,
+        "node": true
+    }
+};
+   
